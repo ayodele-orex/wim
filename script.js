ed1fd9dd8996978246925dce3042d6b0dd0f83cf
ayodele-orex <98238478+ayodele-orex@users.noreply.github.com>
2022-08-25 11:51:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+  searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  searchForm.classList.remove('active');
+  navbar.classList.remove('active');
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
